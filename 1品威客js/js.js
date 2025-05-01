e7d09214cddec1239774744a376a4316cacb5ebf
666zy666 <131882431+666zy666@users.noreply.github.com>
2025-05-01 16:47:01 +0800

    Add files via upload
    signnature加密更新，更新验证码的获取，完整的登录过程

--- 1品威客js/js.js
+++ 1品威客js/js.js
@@ -1,8 +1,9 @@
-const cryptoJs = require('crypto-js')
-// D = parseInt((new Date).getTime() / 1e3);
-        l = {
-            key: cryptoJs.enc.Utf8.parse("fX@VyCQVvpdj8RCa"),
-            iv: cryptoJs.enc.Utf8.parse(function(t) {
+//一品威客.js：
+var crypto = require("crypto");
+var  Cryjs= require("crypto-js");
+l = {
+            key: Cryjs.enc.Utf8.parse("fX@VyCQVvpdj8RCa"),
+            iv: Cryjs.enc.Utf8.parse(function(t) {
                 for (var e = "", i = 0; i < t.length - 1; i += 2) {
                     var n = parseInt(t[i] + "" + t[i + 1], 16);
                     e += String.fromCharCode(n)
@@ -12,20 +13,20 @@ const cryptoJs = require('crypto-js')
         }
           , v = function(data) {
             return function(data) {
-                return cryptoJs.AES.encrypt(data, l.key, {
+                return Cryjs.AES.encrypt(data, l.key, {
                     iv: l.iv,
-                    mode: cryptoJs.mode.CBC,
-                    padding: cryptoJs.pad.Pkcs7
+                    mode: Cryjs.mode.CBC,
+                    padding: Cryjs.pad.Pkcs7
                 }).toString()
             }(data)
         }
           , d = function(data) {
-            return cryptoJs.MD5(data).toString()
+            return Cryjs.MD5(data).toString()
         }
           , f = function(t) {
             var e = "";
             return Object.keys(t).sort().forEach((function(n) {
-                e += n + ("object" === typeof (t[n]) ? JSON.stringify(t[n], (function(t, e) {
+                e += n + ("object" === typeof(t[n]) ? JSON.stringify(t[n], (function(t, e) {
                     return "number" == typeof e && (e = String(e)),
                     e
                 }
@@ -34,7 +35,7 @@ const cryptoJs = require('crypto-js')
             )),
             e
         }
-          , h = function (t) {
+          , h = function(t) {
             var data = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : {}
               , e = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : "a75846eb4ac490420ac63db46d2a03bf"
               , n = e + f(data) + f(t) + e;
@@ -42,23 +43,29 @@ const cryptoJs = require('crypto-js')
             n = v(n)
         }
 
-//         U={
-//     "App-Ver": "",
-//     "Os-Ver": "",
-//     "Device-Ver": "",
-//     "Imei": "",
-//     "Access-Token": "",
-//     "Timestemp": D,
-//     "NonceStr": "1745921263fdww0",
-//     "App-Id": "4ac490420ac63db4",
-//     "Device-Os": "web",
-//     "Signature": "GxVK4R5KanOTW7YdaOsI427JxkjLV4+7DEOJ39g3H6BmQydWxHF/de1ZIuzkgZEf"
-// }
-// M={
-//     "username": "qwqw",
-//     "password": "qwqw",
-//     "code": "",
-//     "hdn_refer": "https://www.epwk.com/"
-// }
-// C='a75846eb4ac490420ac63db46d2a03bf'
-// // console.log(h(U,M,C))
+function fn(L){
+    let D = parseInt((new Date).getTime() / 1e3)
+    var U =
+        {
+    "App-Ver": "",
+    "Os-Ver": "",
+    "Device-Ver": "",
+    "Imei": "",
+    "Access-Token": "",
+    "Timestemp": D+"",
+    "NonceStr": "".concat(D).concat('u1gq2'),
+    "App-Id": "4ac490420ac63db4",
+    "Device-Os": "web"
+}
+
+    U['Signature'] = h(U,L, "a75846eb4ac490420ac63db46d2a03bf")
+    return U;
+}
+
+// 传入参数：
+// var L = {
+//     code: "",
+//     hdn_refer: "https://www.epwk.com/",
+//     password: "1234567899",
+//     username: "15819657566"  }
+// console.log(fn(L))
--- 1品威客js/login.py
+++ 1品威客js/login.py
@@ -1,83 +1,66 @@
-import time
+# import subprocess
+# from functools import partial
+# subprocess.Popen = partial(subprocess.Popen, encoding="utf-8")
 import execjs
 import requests
-with open("js.js", mode="r", encoding="utf-8") as f:
+import json
+
+import time
+import requests
+import execjs
+import ddddocr
+with open(r"C:\Users\郑曦\PycharmProjects\pythonProject1\1品威客js\js.js", mode="r", encoding="utf-8") as f:
     exec_js = f.read()
 exec_code = execjs.compile(exec_js)
 D=str(int(time.time()))
-U={
-    "App-Ver": "",
-    "Os-Ver": "",
-    "Device-Ver": "",
-    "Imei": "",
-    "Access-Token": "",
-    "Timestemp": D,
-    "NonceStr": "1745921263fdww0",
-    "App-Id": "4ac490420ac63db4",
-    "Device-Os": "web",
-    "Signature": "GxVK4R5KanOTW7YdaOsI427JxkjLV4+7DEOJ39g3H6BmQydWxHF/de1ZIuzkgZEf"
-}
-M={
-    "username": "qwqw",
-    "password": "qwqw",
-    "code": "",
-    "hdn_refer": "https://www.epwk.com/"
-}
-C='a75846eb4ac490420ac63db46d2a03bf'
+M={}
 # 调用 JavaScript 中的 test 函数，传入参数并打印结果
-result = exec_code.call('h',U,M,C)
+headers = exec_code.call("fn", M)
 
+
+params = {
+    'channel': 'common_channel',
+    'base64': '1',
+}
 cookies = {
-    'Hm_lvt_3PeMcXBypTMJrVhFndZ19WzpWRLFKvg7te': '1745920335',
-    'HMACCOUNT': 'CFD8D7C57161D566',
-    'HWWAFSESID': 'fb527735e8783d7077',
-    'HWWAFSESTIME': '1745920248513',
-    'PHPSESSID': '7aa393c37541de5a0218162551dc8b737ce91d14',
-    'time_diff': '87',
     'XDEBUG_SESSION': 'XDEBUG_ECLIPSE',
-    'banners_show_cookie_ip': '218.19.30.100',
-    'adbanner_city': '%E5%B9%BF%E5%B7%9E%E5%B8%82',
-    'login_referer': 'https%3A%2F%2Fwww.epwk.com%2F',
-    'Hm_lpvt_3PeMcXBypTMJrVhFndZ19WzpWRLFKvg7te': '1745920338',
+    'HWWAFSESID': 'fe00b73cf13681d906',
+    'HWWAFSESTIME': '1746087590981',
 }
 
-headers = {
-    'Accept': 'application/json, text/plain, */*',
+header = {
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
     'Accept-Language': 'zh-CN,zh;q=0.9',
-    'Access-Token': '',
-    'App-Id': '4ac490420ac63db4',
-    'App-Ver': '',
-    'CHOST': 'www.epwk.com',
     'Cache-Control': 'no-cache',
     'Connection': 'keep-alive',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Device-Os': 'web',
-    'Device-Ver': '',
-    'Imei': '',
-    'NonceStr': '1745920268av3u3',
-    'Origin': 'https://www.epwk.com',
-    'Os-Ver': '',
     'Pragma': 'no-cache',
     'Referer': 'https://www.epwk.com/login.html',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
-    'Signature': result,
-    'Timestemp': D,
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
-    'X-REQUEST-ID': '65f2dca1cc94e02af55aaf3e60b6ab85',
+    'X-Requested-With': 'XMLHttpRequest',
     'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
-    # 'Cookie': 'Hm_lvt_3PeMcXBypTMJrVhFndZ19WzpWRLFKvg7te=1745920335; HMACCOUNT=CFD8D7C57161D566; HWWAFSESID=fb527735e8783d7077; HWWAFSESTIME=1745920248513; PHPSESSID=7aa393c37541de5a0218162551dc8b737ce91d14; time_diff=87; XDEBUG_SESSION=XDEBUG_ECLIPSE; banners_show_cookie_ip=218.19.30.100; adbanner_city=%E5%B9%BF%E5%B7%9E%E5%B8%82; login_referer=https%3A%2F%2Fwww.epwk.com%2F; Hm_lpvt_3PeMcXBypTMJrVhFndZ19WzpWRLFKvg7te=1745920338',
+    # 'Cookie': 'XDEBUG_SESSION=XDEBUG_ECLIPSE; HWWAFSESID=fe00b73cf13681d906; HWWAFSESTIME=1746087590981',
 }
-
+session = requests.session()
+res = session.get("https://www.epwk.com/api/epwk/v1/index/time/",cookies=cookies,headers=header)
+response = session.get('https://www.epwk.com/api/epwk/v1/captcha/show', params=params, headers=headers)
+content = response.json()
+base = content['data']['base64']
+ocr= ddddocr.DdddOcr(show_ad=False)
+code = ocr.classification(base)
+url='https://www.epwk.com/api/epwk/v1/user/login'
 data = {
-    'username': 'qwqw',
-    'password': 'qwqw',
-    'code': '',
-    'hdn_refer': 'https://www.epwk.com/',
+    "code": code,
+    "hdn_refer": "",
+    "password": "111",
+    "username": "111",
 }
-response = requests.post('https://www.epwk.com/api/epwk/v1/user/login', cookies=cookies, headers=headers, data=data)
+session.headers = exec_code.call("fn", data)
+session.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+resp = session.post(url, data=data)
+print(resp.text)
 
-print(response.text)
